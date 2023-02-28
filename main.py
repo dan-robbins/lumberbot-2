6dae5f15c7b8c71cb0f12061fd6a6cd5721bfaab
Dan Robbins <drobbins@terpmail.umd.edu>
2023-02-27 19:55:59 -0500

    add add_cogs function

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 
+async def add_cogs(bot: commands.Bot):
+    await bot.add_cog(music_cog(bot))
+
 bot = commands.Bot(command_prefix='music.', description='Yet another music bot.', intents=intents)
 
 @bot.event
 async def on_ready():
-    await bot.add_cog(music_cog(bot))
+    await add_cogs(bot)
     print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(bot))
 
 bot.run(TOKEN)
