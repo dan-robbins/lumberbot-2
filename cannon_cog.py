799d3a2cb2ec2cd79776dbfee666819b98eb8e9b
Dan Robbins <drobbins@terpmail.umd.edu>
2023-02-28 20:44:25 -0500

    add basic cannon sound effect in cannon_cog

--- cannon_cog.py
+++ cannon_cog.py
@@ -1,3 +1,5 @@
+import asyncio
+import discord
 from discord.ext import commands
 
 class cannon_cog(commands.Cog):
@@ -7,6 +9,19 @@ class cannon_cog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        await self.bot.get_user(int(self.owner_id)).send("Member {}/{} left {}!".format(member.name, member.nick, member.guild.name))
-
-        #TODO add cannon sound
+        await self.bot.get_user(self.owner_id).send("Member {}/{} left {}!".format(member.name, member.nick, member.guild.name))
+        max_users = 0
+        max_channel = None
+        for channel in member.guild.voice_channels:
+            num_users = len(channel.members)
+            if num_users > max_users:
+                max_users = num_users
+                max_channel = channel
+        
+        voice_client = await max_channel.connect()
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("sounds/cannon3.mp3"))
+        voice_client.play(source)
+        while voice_client.is_playing():
+            await asyncio.sleep(1)
+        
+        await voice_client.disconnect()
