9be74101376aadad5ad868faad2cb3eb7c307d09
Dan Robbins <drobbins@terpmail.umd.edu>
2023-03-16 14:51:43 -0400

    Add livecounter command

--- livecounter_cog.py
+++ livecounter_cog.py
@@ -15,3 +15,14 @@ class livecounter_cog(commands.Cog):
             with open('records.json', 'r') as openfile:
                 records = json.load(openfile)
             await message.channel.send("A total of {} woods since January 18th 2018, with a record of {} woods on a single post. A total of {} Danny ace{}.".format(records["woods"], records["record"], records["aces"], '' if int(records["aces"]) == 1 else 's'))
+            return
+
+    @commands.command(name='livecounter')
+    async def _ping(self, ctx: commands.Context):
+        """Print the current livecounter.
+        """
+        
+        with open('records.json', 'r') as openfile:
+            records = json.load(openfile)
+        await ctx.message.channel.send("A total of {} woods since January 18th 2018, with a record of {} woods on a single post. A total of {} Danny ace{}.".format(records["woods"], records["record"], records["aces"], '' if int(records["aces"]) == 1 else 's'))
+        return
