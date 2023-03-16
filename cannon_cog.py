3b7d91d196dd8bf93b8990134b201189bb606321
Dan Robbins <drobbins@terpmail.umd.edu>
2023-03-16 15:33:13 -0400

    Update cannon cog

--- cannon_cog.py
+++ cannon_cog.py
@@ -10,20 +10,35 @@ class cannon_cog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        await self.bot.get_user(self.owner_id).send("Member {}#{}/{} left {}!".format(member.name, member.discriminator, member.nick, member.guild.name))
-        max_users = 0
-        max_channel = None
-        for channel in member.guild.voice_channels:
-            num_users = len(channel.members)
-            if num_users > max_users:
-                max_users = num_users
-                max_channel = channel
-        
-        voice_client = await max_channel.connect()
+        await self.bot.get_user(self.owner_id).send("Member {}#{}/{} left {}!".format(member.name, member.discriminator, member.nick if member.nick is not None else member.name, member.guild.name))
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=member.guild)
+        if not (voice_client and voice_client.is_connected()):
+            max_users = 0
+            max_channel = None
+            for channel in member.guild.voice_channels:
+                num_users = len(channel.members)
+                if num_users > max_users:
+                    max_users = num_users
+                    max_channel = channel
+            
+            voice_client = await max_channel.connect()
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(random.choice(["sounds/cannon2.mp3", "sounds/cannon3.mp3", "sounds/cannon4.mp3", "sounds/cannon5.mp3"])))
         voice_client.play(source)
-        while voice_client.is_playing():
-            await asyncio.sleep(1)
-        
-        await asyncio.sleep(60)
-        await voice_client.disconnect()
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if not member.id == self.bot.user.id:
+            return
+
+        elif before.channel is None:
+            voice = after.channel.guild.voice_client
+            time = 0
+            while True:
+                await asyncio.sleep(1)
+                time = time + 1
+                if voice.is_playing() and not voice.is_paused():
+                    time = 0
+                if time == 60:
+                    await voice.disconnect()
+                if not voice.is_connected():
+                    break
